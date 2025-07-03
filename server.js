3242e0e52d8a7f9dd104c3bf4a613b22e413fb9c
bernazyzz <zebernardomoniz@gmail.com>
2025-07-03 15:03:18 +0100

    Logging Express

--- server.js
+++ server.js
@@ -15,6 +15,15 @@ const friends = [
   },
 ];
 
+app.use((req, res, next) => {
+  const start = Date.now();
+  next();
+  // actions go here...
+  const delta = Date.now() - start;
+  console.log(`${req.method} ${req.url} ${delta}ms`);
+
+});
+
 app.get("/friends", (req, res) => {
   res.json(friends);
 });
